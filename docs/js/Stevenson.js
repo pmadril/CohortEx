51bdf94d55789fa38523970bd5b3dab222206ae9
Maria Tereza Fernandes Abrahao <terezaabrahao@hotmail.com>
2017-01-22 13:46:20 -0200

    - Error: Stevenson.Account. was missed.

--- docs/js/Stevenson.js
+++ docs/js/Stevenson.js
@@ -71,7 +71,7 @@ var Stevenson = {
 		});
 
 		Stevenson.log.debug("Loading the global CMS template");
-		$.Mustache.load(siteBaseURL + '/templates/cms.html').done(function(){		
+		$.Mustache.load(Stevenson.Account.siteBaseURL + '/templates/cms.html').done(function(){		
 			Stevenson.log.info('Initializing application');
 
 			// Pre-start checks
@@ -94,7 +94,7 @@ var Stevenson = {
 			}else{
 				if (Stevenson.Account.authenticated && Stevenson.Account.authenticated == true) {
 					Stevenson.log.debug("Adding logged in top section");
-					$.Mustache.load(siteBaseURL + '/templates/authentication.html').done(function () {
+					$.Mustache.load(Stevenson.Account.siteBaseURL + '/templates/authentication.html').done(function () {
 						$('#top-login').html('');
 						$('#top-login').mustache('top-bar', {name: Stevenson.Account.name});
 					});
@@ -675,7 +675,7 @@ var Stevenson = {
 					}
 				}
 
-				$.Mustache.load(siteBaseURL + '/templates/cms.html').done(function () {
+				$.Mustache.load(Stevenson.Account.siteBaseURL + '/templates/cms.html').done(function () {
 					Stevenson.ui.ContentEditor.currentEditor.setContent(page);
 				});
 			},
@@ -787,7 +787,7 @@ var Stevenson = {
 						new EpicEditor({
 							textarea: 'content',
 							container: 'markdown-editor',
-							basePath: siteBaseURL + '/js/epiceditor',
+							basePath: Stevenson.Account.siteBaseURL + '/js/epiceditor',
 							autogrow: true
 						}).load();
 					},
