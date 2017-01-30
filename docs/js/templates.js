72f01fde3de474f0d9add2a1144ec295f3765a17
Maria Tereza Fernandes Abrahao <terezaabrahao@hotmail.com>
2017-01-30 00:17:06 -0200

    - Adjust paths so it works under docs

--- docs/js/templates.js
+++ docs/js/templates.js
@@ -1,22 +1,26 @@
 (function($) {
 	var loadTemplates = function(path){
 		Stevenson.ui.Loader.display('Loading templates...', 100);
+		layoutsPathWOLastSlash = Stevenson.Account.layoutsPath.substring(0,Stevenson.Account.layoutsPath.lastIndexOf('/'));
+		editorsPathWOLastSlash = Stevenson.Account.editorsPath.substring(0,Stevenson.Account.editorsPath.lastIndexOf('/'));
+		schemasPathWOLastSlash = Stevenson.Account.schemasPath.substring(0,Stevenson.Account.schemasPath.lastIndexOf('/'));
+		
 		Stevenson.repo.getFiles({
-			path: '_layouts',
+			path: layoutsPathWOLastSlash, //'_layouts',
 			success: function(layouts){
 				$('#templates').html('');
 				Stevenson.repo.getFiles({
-					path: '_editors',
+					path: editorsPathWOLastSlash, //'_editors',
 					success: function(editors){
 						$.each(layouts, function(index, layout){
-							if(layout.path.indexOf('_layouts') != -1 && layout.path != '_layouts'){
+							if(layout.path.indexOf('_layouts') != -1 && layout.path != layoutsPathWOLastSlash){ //'_layouts'
 								Stevenson.log.info('Adding layout: '+layout.path);
-								var id = layout.path.replace('.html','').replace('_layouts/','');
+								var id = layout.path.replace('.html','').replace(Stevenson.Account.layoutsPath,'');
 								layout.id = id;
-								layout.editorurl = '/cms/edit.html?new=true#_editors/' + id + '.json';
+								layout.editorurl = '/cms/edit.html?new=true#' + Stevenson.Account.editorsPath + id + '.json'; //_editors/
 								for(var i=0;i<editors.length;i++){
 									if(editors[i].path == '_editors/'+id+'.json'){
-										layout.editorurl = '/cms/edit.html#_editors/' + id + '.json';
+										layout.editorurl = '/cms/edit.html#' + Stevenson.Account.editorsPath + id + '.json'; //_editors/
 										break;
 									}
 								}
@@ -37,21 +41,21 @@
 			}
 		});
 		Stevenson.repo.getFiles({
-			path: '_schemas',
+			path: Stevenson.Account.schemasPath, //'_schemas',
 			success: function(schemas){
 				$('#templates').html('');
 				Stevenson.repo.getFiles({
-					path: '_schemas',
+					path: schemasPathWOLastSlash, //'_schemas',
 					success: function(schemas){
 						$.each(schemas, function(index, schema){
-							if(schema.path.indexOf('_schemas') != -1 && schema.path != '_schemas'){
+							if(schema.path.indexOf('_schemas') != -1 && schema.path != schemasPathWOLastSlash){ //'_schemas'
 								Stevenson.log.info('Adding schema: '+schema.path);
-								var id = schema.path.replace('.html','').replace('_schemas/','');
+								var id = schema.path.replace('.html','').replace(Stevenson.Account.schemasPath,''); //'_schemas/'
 								schema.id = id;
-								schema.schemaurl = '/cms/edit.html?new=true#_schemas/' + id + '.json';
+								schema.schemaurl = '/cms/edit.html?new=true#' + Stevenson.Account.schemasPath + id + '.json'; // _schemas/
 								for(var i=0;i<schemas.length;i++){
-									if(schemas[i].path == '_schemas/'+id+'.json'){
-										schema.schemaurl = '/cms/edit.html#_schemas/' + id + '.json';
+									if(schemas[i].path == Stevenson.Account.schemasPath + id+'.json'){ //'_schemas/'
+										schema.schemaurl = '/cms/edit.html#' + Stevenson.Account.schemasPath + id + '.json'; //_schemas/
 										break;
 									}
 								}
@@ -80,7 +84,7 @@
 			if(templateName.indexOf('.html') == -1){
 				templateName += '.html';
 			}
-			window.location = '/cms/edit.html?new=true#_layouts/'+templateName;
+			window.location = '/cms/edit.html?new=true#' + Stevenson.Account.layoutsPath +templateName; //_layouts/
 			return false;
 		});
 		$('.breadcrumb .repo').html(Stevenson.Account.repo);
