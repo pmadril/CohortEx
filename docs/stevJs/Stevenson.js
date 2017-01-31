472eb6de6135e13a32ad4bfa42bfcc44a99b493c
Maria Tereza Fernandes Abrahao <terezaabrahao@hotmail.com>
2017-01-31 13:28:37 -0200

    - Another way to set document_base_url. The other didn’t work.

--- docs/stevJs/Stevenson.js
+++ docs/stevJs/Stevenson.js
@@ -18,6 +18,7 @@ var Stevenson = {
 		editorsFolder: '_editors',
 		templatesFolder: 'templates',
 		schemaExtension: '.jctx',
+        documentBaseURL: '{{ site.url}}{{site.baseurl}}'
 		siteBaseURL: '{{ site.baseurl }}',
 		username : '',
 		/**
